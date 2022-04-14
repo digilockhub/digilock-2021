81ee668ce5eb746c7cb86b04ca4624073dee6bba
Dan Wade <daniel.wade@digilock.com>
2022-04-14 09:49:04 -0700

    lang switcher using locales list

--- site/src/components/locales/language-switcher.js
+++ site/src/components/locales/language-switcher.js
@@ -1,30 +1,42 @@
 
 import React from 'react';
 import {Link, useI18next} from 'gatsby-plugin-react-i18next';
+import locales from '../../constants/locales/locales'
 
 const LanguageSwitcher = () => {
   const {languages, originalPath} = useI18next();
+
+  console.log('languages', languages);
+  console.log('originalPath', originalPath);
+
+  let localesArr = Object.keys(locales).map(function(key) {
+    return locales[key];
+  });
+
+  function createOptionLabel(ind) {
+    if(localesArr[ind].locale === 'English') {
+      return localesArr[ind].locale;
+    } else {
+      return localesArr[ind].locale + ' | '+localesArr[ind].native;
+    }
+  }
+
   return (
 
       <div className={'custom-select'}>
         <select name="" id="">
-          <option value="zh">Chinese | 中文</option>
-          <option value="nl">Dutch | Nederlands</option>
-          <option selected="" value="en">English</option>
-          <option value="fr">French | Français</option>
-          <option value="de">German | Deutsche</option>
-          <option value="ja">Japanese | 日本語</option>
-          <option value="ko">Korean | 한국어</option>
-          <option value="pt">Portuguese | Português</option>
-          <option value="es">Spanish | Español</option>
-          <option value="tr">Turkish | Türk</option>
-        {/*{languages.map((lng) => (*/}
-        {/*    <option key={lng} value={''}>*/}
-        {/*      {'English'}*/}
-        {/*    </option>*/}
-        {/*))}*/}
+          {
+            localesArr.map(function(locale, index) {
+              return <option
+                  key={index}
+                  value={localesArr[index].path}
+              >
+                {createOptionLabel(index)}
+              </option>
+            })};
+          }
         </select>
-        <span className="custom-arrow"></span>
+        <span className="custom-arrow" />
       </div>
 
   )
@@ -32,21 +44,3 @@ const LanguageSwitcher = () => {
 };
 
 export default LanguageSwitcher;
-
-
-{/*<div className="custom-select">*/}
-{/*  <select name="" id="">*/}
-{/*    <option value="">English</option>*/}
-{/*    <option value="">EnglishEnglish</option>*/}
-{/*    <option value="">Item 3</option>*/}
-{/*    <option value="">Item 4</option>*/}
-{/*    <option value="">Item 5</option>*/}
-{/*  </select>*/}
-{/*  <span className="custom-arrow"></span>*/}
-{/*</div>*/}
-
-{/*<Link to={originalPath} language={lng}>*/}
-{/*  {*/}
-{/*    lng*/}
-{/*  }*/}
-{/*</Link>*/}
