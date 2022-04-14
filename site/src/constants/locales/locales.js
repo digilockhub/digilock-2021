894151bcb4e9861c4178d37ee511f9eb7d084ff5
Dan Wade <daniel.wade@digilock.com>
2022-04-14 09:34:25 -0700

    initial add

---
+++ site/src/constants/locales/locales.js
@@ -0,0 +1,53 @@
+module.exports = {
+  zh: {
+    path: 'zh',
+    locale: 'Chinese',
+    native: '中文'
+  },
+  nl: {
+    path: 'nl',
+    locale: 'Dutch',
+    native: 'Nederlands'
+  },
+  en: {
+    path: 'en',
+    locale: 'English',
+    default: true,
+    native: ''
+  },
+  fr: {
+    path: 'fr',
+    locale: 'French',
+    native: 'Français'
+  },
+  de: {
+    path: 'de',
+    locale: 'German',
+    native: 'Deutsche'
+  },
+  ja: {
+    path: 'ja',
+    locale: 'Japanese',
+    native: '日本語'
+  },
+  ko: {
+    path: 'ko',
+    locale: 'Korean',
+    native: '한국어'
+  },
+  pt: {
+    path: 'pt',
+    locale: 'Portuguese',
+    native: 'Português'
+  },
+  es: {
+    path: 'es',
+    locale: 'Spanish',
+    native: 'Español'
+  },
+  tr: {
+    path: 'tr',
+    locale: 'Turkish',
+    native: 'Türk'
+  },
+};
