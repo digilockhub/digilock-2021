b87b4527ce0e6662b2eb3ee2baf05c0354f3be83
Dan Wade <daniel.wade@digilock.com>
2022-04-20 09:24:20 -0700

    industries grid

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -3,9 +3,11 @@ import {Link, Trans, useTranslation} from 'gatsby-plugin-react-i18next';
 import {StaticImage} from "gatsby-plugin-image";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import { graphql } from 'gatsby';
+import {graphql} from 'gatsby';
 import BCResponsivePlayerDynId from '../components/video-player/BCResponsivePlayerDynId'
 import Modal from '../components/modals/modal'
+
+import indWorkspace from '../images/home/industries/workspace.jpg'
 
 
 import clientLogos from '../images/home/client-logos/logos_group.png'
@@ -14,53 +16,53 @@ const IndexPage = () => {
   const [showModal, setShowModal] = useState(false);
 
   function handleShowModal() {
-    setShowModal( prevShowModal =>!prevShowModal)
+    setShowModal(prevShowModal => !prevShowModal)
   }
 
   const {t} = useTranslation();
   return (
       <Layout>
-        <Seo title={t('Home')} />
+        <Seo title={t('Home')}/>
         <div className="home">
           <section className="hero">
 
-              <BCResponsivePlayerDynId
-                  vid={'6302604050001'}
-                  dynId={'HeroVideo'}
-                  translation={t('hero_video_caption')}
-              />
+            <BCResponsivePlayerDynId
+                vid={'6302604050001'}
+                dynId={'HeroVideo'}
+                translation={t('hero_video_caption')}
+            />
 
           </section>
           <section className={'lock-solutions'}>
             <div className="container">
 
 
-                <h1>
-                  <Trans>section_one_headline</Trans>
-                </h1>
-                <div className={'grid'}>
-                  <div className="col copy-container right">
-                    <h2>
-                      <Trans>section_one_headline_2</Trans>
-                    </h2>
-                    <p>
-                      <Trans>section_one_headline_2_copy</Trans>
-                    </p>
-                  </div>
-                  <div className="delimeter" />
-                  <div className="col copy-container left">
-                    <h2>
-                      <Trans>section_one_headline_3</Trans>
-                    </h2>
-                    <p>
-                      <Trans>section_one_headline_3_copy</Trans>
-                    </p>
-                  </div>
+              <h1>
+                <Trans>section_one_headline</Trans>
+              </h1>
+              <div className={'grid'}>
+                <div className="col copy-container right">
+                  <h2>
+                    <Trans>section_one_headline_2</Trans>
+                  </h2>
+                  <p>
+                    <Trans>section_one_headline_2_copy</Trans>
+                  </p>
                 </div>
+                <div className="delimeter"/>
+                <div className="col copy-container left">
+                  <h2>
+                    <Trans>section_one_headline_3</Trans>
+                  </h2>
+                  <p>
+                    <Trans>section_one_headline_3_copy</Trans>
+                  </p>
+                </div>
+              </div>
 
             </div>
           </section>
-          <div className="section-delimeter container" />
+          <div className="section-delimeter container"/>
           <section className="clients">
             <div className="container">
               <h2>
@@ -71,20 +73,57 @@ const IndexPage = () => {
               </div>
             </div>
           </section>
-          <div className="section-delimeter container" />
+          <div className="section-delimeter container"/>
           <section className="industries">
             <div className="container">
-              <h2>Solutions tailored to your industry</h2>
-              <div className="columns">
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
-                <div className="col btn-industry">k</div>
+              <h1>Solutions tailored to your industry</h1>
+              <div className="industries-grid">
+                <Link to={'#'}>
+                  <div className="col btn-industry workspace">
+                    <span><Trans>workspace</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry education">
+                    <span><Trans>education</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry health-fitness">
+                    <span><Trans>health_fitness</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry healthcare">
+                    <span><Trans>healthcare</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry retail">
+                    <span><Trans>retail</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry hospitality">
+                    <span><Trans>hospitality</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry pro-sports">
+                    <span><Trans>pro_sports</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry manufacturing">
+                    <span><Trans>manufacturing</Trans></span>
+                  </div>
+                </Link>
+                <Link to={'#'}>
+                  <div className="col btn-industry government">
+                    <span><Trans>government</Trans></span>
+                  </div>
+                </Link>
+
               </div>
             </div>
           </section>
