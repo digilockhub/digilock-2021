6eb3720970889f4e15b388aabb4de6aac5980cd7
Dan Wade <daniel.wade@digilock.com>
2022-04-21 14:26:46 -0700

    footer day. using grid for first time. might work!!

--- site/src/components/footer.js
+++ site/src/components/footer.js
@@ -1,42 +1,174 @@
 import React, {useState} from 'react';
-import { graphql } from 'gatsby';
+import {graphql} from 'gatsby';
 import {Link, Trans, useTranslation} from 'gatsby-plugin-react-i18next';
+import companySettings from '../constants/company/settings'
+
+import linkedIn from '../images/shared/linkedin.svg';
+import youtube from '../images/shared/youtube.svg';
 
 
 const Footer = () => {
   const {t} = useTranslation();
-  const [count, setCount] = useState(0);
 
   return (
 
       <footer>
-        <div className="container grid">
-          {/*<div className="col">*/}
-          {/*  <h3>*/}
-          {/*    <Trans>have_questions</Trans>*/}
-          {/*  </h3>*/}
-          {/*  <a href="#" className="btn btn--trans white">*/}
-          {/*    <Trans>button_chat</Trans>*/}
-          {/*  </a>*/}
-          {/*  <a href="#" className="btn btn--trans white">*/}
-          {/*    <Trans>button_contact</Trans>*/}
-          {/*  </a>*/}
-          {/*  <a href="#" className="btn btn--trans white">*/}
-          {/*    <Trans>button_faq</Trans>*/}
-          {/*  </a>*/}
+        <div className="container">
+          <h3>
+            <Trans>have_questions</Trans>
+          </h3>
+          <div className="questions">
+            <a href="#" className="btn btn--trans white">
+              <Trans>button_chat</Trans>
+            </a>
+            <a href="#" className="btn btn--trans white">
+              <Trans>button_contact</Trans>
+            </a>
+            <a href="#" className="btn btn--trans white">
+              <Trans>button_faq</Trans>
+            </a>
+          </div>
+          <div className="locations">
+            <div className="location americas">
+              <h4>Digilock Americas</h4>
+              <ul className={'unstyled-list'}>
+                <li>Petaluma, Ca | United States</li>
+                <li><span>Contact Sales / US</span></li>
+                <li>
+                  <a href="mailto:sales@digilock.com">sales@digilock.com</a>
+                </li>
+                <li>Telephone +1 707-766-6000</li>
+                <li>Toll-Free 800-989-0201</li>
+              </ul>
+              <div className="social-icons">
+                <h4>Follow U.S</h4>
+                <a href="#">
+                  <img src={linkedIn} alt="follow us on linkedin"/>
+                </a>
+                <a href="#">
+                  <img src={youtube} alt="follow us on YouTube"/>
+                </a>
+              </div>
+            </div>
+            <div className="location showroom">
+              <h4>Digilock Showroom</h4>
+              <ul className={'unstyled-list'}>
+                <li>theMart | Chicago, IL 60654</li>
+                <li><span><a href="#">Discover Our Showroom</a></span></li>
+                <li>
+                  <a href="mailto:sales@digilock.com">sales@digilock.com</a>
+                </li>
+                <li>Telephone +1 707-766-6000</li>
+                <li>Toll-Free 800-989-0201</li>
+              </ul>
+            </div>
 
-          {/*</div>*/}
-
+            <div className="location americas">
+              <h4>Digilock Europe BV</h4>
+              <ul className={'unstyled-list'}>
+                <li>The Netherlands</li>
+                <li><span>Contact Sales / Europe</span></li>
+                <li>Telephone +31 0 20 303 3060</li>
+              </ul>
+              <div className="social-icons">
+                <h4>Follow Europe</h4>
+                <a href="#">
+                  <img src={linkedIn} alt="follow us on linkedin"/>
+                </a>
+                <a href="#">
+                  <img src={youtube} alt="follow us on YouTube"/>
+                </a>
+              </div>
+            </div>
+            <div className="location americas">
+              <h4>Digilock Asia</h4>
+              <ul className={'unstyled-list'}>
+                <li>Hong Kong</li>
+                <li><span>Contact Sales / Asia</span></li>
+                <li>Telephone +852 27776 0800</li>
+              </ul>
+              <div className="social-icons">
+                <h4>Follow Asia</h4>
+                <a href="#">
+                  <img src={linkedIn} alt="follow us on linkedin"/>
+                </a>
+                <a href="#">
+                  <img src={youtube} alt="follow us on YouTube"/>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="section-delimeter"/>
+          <div className="customer-care">
+            <div>
+              <h4><Trans>customer_care</Trans></h4>
+              <ul className={'unstyled-list'}>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>find_dealers</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>support</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>contact</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h4>Digilock</h4>
+              <ul className={'unstyled-list'}>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>careers</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>about</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>mission</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>blog</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>privacy_policy</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>privacy_request_form</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>manage_cookies</Trans>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'#'}>
+                    <Trans>patents</Trans>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
           <p>
             <sup>&copy;</sup> {new Date().getFullYear()} Footer
           </p>
-          {/*<p>*/}
-          {/*  You clicked {count} times*/}
-          {/*</p>*/}
-          {/*<button onClick={() => setCount(count+1)}>*/}
-          {/*  /!*<Trans>button_label</Trans>*!/*/}
-          {/*  {t('button_label')}*/}
-          {/*</button>*/}
         </div>
       </footer>
 
---
+++ site/src/constants/company/settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  CONTACT_FORM_ENDPOINT: 'https://api.hsforms.com/submissions/v3/integration/submit/6919241/26ffb146-501d-48ce-b599-388fc6318057',
+  CONTACT_FORM_PACKAGEHOLD_ENDPOINT: 'https://api.hsforms.com/submissions/v3/integration/submit/6919241/d7c64fbb-b040-40bb-9572-4d9e57444f20',
+  CONTACT_FORM_PRIVACY: 'https://api.hsforms.com/submissions/v3/integration/submit/6919241/84bb0e4e-122a-4373-930b-e82303ed58f1',
+  MARKETING_EMAIL: 'marketing@digilock.com',
+  SALES_EMAIL: 'sales@digilock.com',
+  SUPPORT_EMAIL: 'support@digilock.com',
+  PRIVACY_EMAIL: 'julie.advocate@digilock.com'
+};
