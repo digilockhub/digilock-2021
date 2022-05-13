76ac3e3a42688fcbaff1a7c5991f6d52b427e592
Dan Wade <daniel.wade@digilock.com>
2022-05-13 11:20:48 -0700

    brook’s comments

--- site/src/components/contact/info-card-current.js
+++ site/src/components/contact/info-card-current.js
@@ -5,7 +5,7 @@ const InfoCardCurrent = (props) => {
   return (
       <div className="info-card">
         <div>
-          <h2>{props.header}</h2>
+          <h3>{props.header}</h3>
           <p dangerouslySetInnerHTML={{__html: props.address}}/>
           <p>
             <span>{props.callSales}</span><br/>
@@ -14,10 +14,10 @@ const InfoCardCurrent = (props) => {
           </p>
         </div>
         <div className={'info-buttons'}>
-          <a href="mailto:sales@digilock.com" className={'btn btn--orange'}>{props.emailSales}</a> <br/>
-          <a href="#" onClick={props.handleLiveChat} className={'btn btn--orange'}>{props.liveChat}</a>
+          <a href="mailto:sales@digilock.com" className={'btn btn--orange email-sales'}>{props.emailSales}</a> <br/>
+          <a href="#" onClick={props.handleLiveChat} className={'btn btn--orange live-chat'}>{props.liveChat}</a>
           <p className={'live-chat-copy'} dangerouslySetInnerHTML={{__html: props.liveChatCopy}} />
-          <a href="#" className={'btn btn--orange'}>FAQ</a>
+          <a href="#" className={'btn btn--orange faq'}>FAQ</a>
         </div>
       </div>
   )
