39024a2893539761b2b2906302bc20c562368872
Dan Wade <daniel.wade@digilock.com>
2022-05-16 10:51:23 -0700

    fixing translations

--- site/src/pages/404.js
+++ site/src/pages/404.js
@@ -1,51 +1,72 @@
 import * as React from "react"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import {Link, Trans} from 'gatsby-plugin-react-i18next';
+import {Link, Trans, useTranslation} from 'gatsby-plugin-react-i18next';
+import {graphql} from "gatsby";
 
 
 const NotFoundPage = () => (
-  <Layout>
-    <Seo title="404: Not found" />
-    <div className="four-oh-four">
-      <section className="hero">
-        <div className="container">
-          <h1>
-            <Trans>
-              Sorry that page, does not exist.
-            </Trans>
-          </h1>
-          <ul className={'unstyled-list'}>
-            <li>
-              <span>QUICK LINKS:</span>
-            </li>
-            <li>
-              <a href="/products/locks/">
-                Locks Overview
-              </a>
-            </li>
-            <li className={'line'}>|</li>
-            <li>
-              <a href="/where-to-buy/">
-                How to Buy
-              </a>
-            </li>
-            <li className={'line'}>|</li>
-            <li>
-              <a href="/support/">
-                Support
-              </a>
-            </li>
-          </ul>
-          <p>
-            Get your questions answered.
-          </p>
-          <a href="#" className={'btn btn--orange email-sales'}>FAQ</a> <br/>
-        </div>
-      </section>
-    </div>
-  </Layout>
+    <Layout>
+      <Seo title="404: Not found"/>
+      <div className="four-oh-four">
+        <section className="hero">
+          <div className="container">
+            <h1>
+              <Trans>
+                does_not_exist
+              </Trans>
+            </h1>
+            <ul className={'unstyled-list'}>
+              <li>
+              <span>
+                <Trans>
+                  quick_links
+                </Trans>:
+              </span>
+              </li>
+              <li>
+                <Link to="/products/locks/">
+                  <Trans>locks_overview</Trans>
+                </Link>
+              </li>
+              <li className={'line'}>|</li>
+              <li>
+                <Link to="/where-to-buy/">
+                  <Trans>how_to_buy</Trans>
+                </Link>
+              </li>
+              <li className={'line'}>|</li>
+              <li>
+                <Link to="/support/">
+                  <Trans>support</Trans>
+                </Link>
+              </li>
+            </ul>
+            <p>
+              <Trans>
+                questions_answered
+              </Trans>
+            </p>
+            <a href="#" className={'btn btn--orange email-sales'}>
+              <Trans>faq</Trans>
+            </a>
+          </div>
+        </section>
+      </div>
+    </Layout>
 )
 
-export default NotFoundPage
+export default NotFoundPage;
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: {language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
