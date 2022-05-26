e43701ce45c1974676c4236bcd4ea369dd255366
Dan Wade <daniel.wade@digilock.com>
2022-05-26 13:36:02 -0700

    blog post

--- site/src/templates/BlogPost.js
+++ site/src/templates/BlogPost.js
@@ -3,39 +3,49 @@ import {graphql, Link} from "gatsby";
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
-import { replaceCharWithChar } from '../Utils/utils'
+import {replaceCharWithChar} from '../Utils/utils';
+import {StaticImage} from "gatsby-plugin-image";
 
 const BlogPostTemplate = (props) => {
 
+  const blogPost = props.pageContext.post;
   const heroImage = props.pageContext.post.feature_image.url;
 
   let blogBody = props.pageContext.post.body;
   blogBody = replaceCharWithChar(blogBody, '<p>&nbsp;</p>', '');
   blogBody = replaceCharWithChar(blogBody, 'style="color: black;"', '');
   blogBody = replaceCharWithChar(blogBody, '<p style="text-align: left; font-size: 18px; font-weight: bold;">&nbsp;</p>', '');
+  blogBody = replaceCharWithChar(blogBody, '<p style="font-size: 16px; line-height: 1;">&nbsp;</p>', '');
   blogBody = replaceCharWithChar(blogBody, '<pre style="font-weight: bold; font-size: 12px;">&nbsp;</pre>', '');
-  blogBody = replaceCharWithChar(blogBody, '<p><span style="color: black;">&nbsp;</span></p>', '');
+  blogBody = replaceCharWithChar(blogBody, '<div><p><span style="color: black;">&nbsp;</span></p></div>', '');
+  blogBody = replaceCharWithChar(blogBody, '<div><span style="font-size: 18px; background-color: transparent; color: #009abf;">&nbsp;</span></div>', '');
   blogBody = replaceCharWithChar(blogBody, '<p style="margin-top: 0in; margin-right: 0in; margin-bottom: 0in; padding-left: 0in;">&nbsp;</p>', '');
 
   return (
 
       <Layout>
         <Seo
-            title={props.pageContext.post.meta.title}
-            description={props.pageContext.post.meta.description}
+            title={blogPost.meta.title}
+            description={blogPost.meta.description}
         />
         <div className="blog-post-container">
-          <section className="hero --hero-image no-pad">
-            <img src={heroImage} alt=""/>
+          <section className="">
+            <div className="container">
+              <img src={heroImage} alt={blogPost.feature_image.alt_text}/>
+            </div>
           </section>
           <section className={'blog-body'}>
-            <h1>
-              {props.pageContext.post.title}
-            </h1>
-            <div dangerouslySetInnerHTML={{__html: blogBody}} />
+            <div className="container">
+              <h1>
+                {blogPost.title}
+              </h1>
+              <div dangerouslySetInnerHTML={{__html: blogBody}}/>
+            </div>
           </section>
           <section className="back-to-blog">
-            <Link to={'/blog/'}>&lt; Back to all Blogs</Link>
+            <div className="container">
+              <Link to={'/blog/'}>&lt; Back to all Blogs</Link>
+            </div>
           </section>
         </div>
       </Layout>
