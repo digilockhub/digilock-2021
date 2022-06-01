1e747054563b80ce12ed8fade4916d9d4a822773
Dan Wade <daniel.wade@digilock.com>
2022-06-01 13:57:42 -0700

    inital axis product page grid

--- site/src/pages/products/electronic-locks/axis/index.js
+++ site/src/pages/products/electronic-locks/axis/index.js
@@ -9,6 +9,7 @@ import BCPlayer360 from '../../../../components/video-player/BCPlayer360';
 import {createMarkup} from '../../../../utils/utils';
 import compareLeft from './images/NKAXSTWPKPBN_360ROT_FF.jpg';
 import compareRight from './images/NKAXSTWPRFBN_360ROT_FF.jpg';
+import {StaticImage} from "gatsby-plugin-image";
 
 
 const Axis = (props) => {
@@ -36,7 +37,7 @@ const Axis = (props) => {
               <h1>Axis Product Details</h1>
               <div className={'grid-two-col'}>
 
-                <div className="interface">
+                <div className="grid-two-col-item">
                   <div className="revealer center-top">
                     <RevealerWrapper
                         compareLeft={compareRight}
@@ -48,10 +49,10 @@ const Axis = (props) => {
                     />
                   </div>
                   <div className="product-copy">
-                    <h2 className={'callout locker'}>
+                    <h2>
                       {t('interface')}
                     </h2>
-                    <h3 className={'front-unit'}>
+                    <h3>
                       {t('front_unit')}
                     </h3>
                     <h3>
@@ -73,15 +74,15 @@ const Axis = (props) => {
                     </p>
                   </div>
                 </div>
-                <div className="body-style">
+                <div className="grid-two-col-item">
                   <div className="video-360">
                     <BCPlayer360 vid={bcVideoId} loc={'nextlock'}  />
                   </div>
                   <div className="product-copy">
-                    <h2 className={'callout locker'}>
+                    <h2>
                       {t('body_style')}
                     </h2>
-                    <h3 className={'front-unit'}>
+                    <h3>
                       {t('front_unit')}
                     </h3>
                     <h3>
@@ -119,6 +120,140 @@ const Axis = (props) => {
                   </div>
                 </div>
 
+              </div>
+            </div>
+          </section>
+          <div className="section-delimeter container" />
+          <section className="rear-units">
+            <div className="container">
+              <div className="grid-two-col">
+                <div className="grid-two-col-item">
+                  <div>
+                    <StaticImage
+                        src={'../../../../images/locks/rear-units/5g-latch.png'}
+                        loading={'lazy'}
+                        width={364}
+                        height={247}
+                        quality={100}
+                        formats={["auto", "webp", "avif"]}
+                        alt="NEED ALT TAG"
+                    />
+                  </div>
+                  <div>
+                    <h2>
+                      {t('deadlatch')}
+                    </h2>
+                    <h3>
+                      {t('rear_unit')}
+                    </h3>
+                    <p>
+                      {t('deadlatch_rear_unit_copy')}
+                    </p>
+                    <h3>
+                      {t('assigned_use')}
+                    </h3>
+                    <p>
+                      {t('deadlatch_assigned_use_copy')}
+                    </p>
+                  </div>
+                </div>
+                <div className="grid-two-col-item">
+                  <div>
+                    <StaticImage
+                        src={'../../../../images/locks/rear-units/5g-bolt.png'}
+                        loading={'lazy'}
+                        width={364}
+                        height={247}
+                        quality={100}
+                        formats={["auto", "webp", "avif"]}
+                        alt="NEED ALT TAG"
+                    />
+                  </div>
+                  <div>
+                    <h2>
+                      {t('deadbolt')}
+                    </h2>
+                    <h3>
+                      {t('rear_unit')}
+                    </h3>
+                    <p>
+                      {t('deadbolt_rear_unit_copy')}
+                    </p>
+                    <h3>
+                      {t('shared_use')}
+                    </h3>
+                    <p>
+                      {t('deadbolt_shared_use_copy')}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <div className="section-delimeter container" />
+          <section className="management">
+            <div className="container">
+              <div className="grid-two-col">
+                <div className="grid-two-col-item">
+                  <div>
+                    <StaticImage
+                        src={'../../../../images/locks/management/app-manage-next.png'}
+                        loading={'lazy'}
+                        width={214}
+                        height={216}
+                        quality={100}
+                        formats={["auto", "webp", "avif"]}
+                        alt="NEED ALT TAG"
+                    />
+                  </div>
+                  <div>
+                    <h2>
+                      {t('deadlatch')}
+                    </h2>
+                    <h3>
+                      {t('rear_unit')}
+                    </h3>
+                    <p>
+                      {t('deadlatch_rear_unit_copy')}
+                    </p>
+                    <h3>
+                      {t('assigned_use')}
+                    </h3>
+                    <p>
+                      {t('deadlatch_assigned_use_copy')}
+                    </p>
+                  </div>
+                </div>
+                <div className="grid-two-col-item">
+                  <div>
+                    <StaticImage
+                        src={'../../../../images/locks/management/app-manage-keys-all.png'}
+                        loading={'lazy'}
+                        width={169}
+                        height={192}
+                        quality={100}
+                        formats={["auto", "webp", "avif"]}
+                        alt="NEED ALT TAG"
+                    />
+                  </div>
+                  <div>
+                    <h2>
+                      {t('deadbolt')}
+                    </h2>
+                    <h3>
+                      {t('rear_unit')}
+                    </h3>
+                    <p>
+                      {t('deadbolt_rear_unit_copy')}
+                    </p>
+                    <h3>
+                      {t('shared_use')}
+                    </h3>
+                    <p>
+                      {t('deadbolt_shared_use_copy')}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
