02cd8408c48733aab31ec5ee243c0062fb86a2d7
Dan Wade <daniel.wade@digilock.com>
2022-06-07 14:57:47 -0700

    modal close button

--- site/src/components/modals/modal.js
+++ site/src/components/modals/modal.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import Header from "../header";
+import {Trans} from "gatsby-plugin-react-i18next";
 const Modal = ({ handleClose, show, children }) => {
 
   const showHideClassName = show ? "modal display-block" : "modal display-none";
@@ -10,9 +11,9 @@ const Modal = ({ handleClose, show, children }) => {
         <section className="modal-main">
           <div className="container">
             {children}
-            <button type="button" onClick={handleClose}>
-              Close
-            </button>
+            <a onClick={handleClose} href="javascript:void(0)" className="btn btn--orange">
+              <Trans>close</Trans>
+            </a>
           </div>
         </section>
       </div>
