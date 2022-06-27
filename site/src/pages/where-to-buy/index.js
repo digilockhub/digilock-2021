06dfaf8d83ce716e9082622ee947d4c851b9ffe6
Dan Wade <daniel.wade@digilock.com>
2022-06-27 15:17:50 -0700

    removing unused

--- site/src/pages/where-to-buy/index.js
+++ site/src/pages/where-to-buy/index.js
@@ -50,66 +50,6 @@ const IndexPage = () => {
                   <Trans>chat</Trans>
                 </a>
               </div>
-              {/*<div className="locations">*/}
-              {/*  <div className="location americas">*/}
-
-              {/*    <h4>Digilock Americas</h4>*/}
-              {/*    <ul className={'unstyled-list'}>*/}
-              {/*      <li>Petaluma, Ca<br />United States</li>*/}
-              {/*      <li>&nbsp;</li>*/}
-              {/*      <li>*/}
-              {/*        <a href={CompanySettings.SALES_EMAIL}>*/}
-              {/*          <Trans>email_sales</Trans> / US*/}
-              {/*        </a>*/}
-              {/*      </li>*/}
-              {/*      <li>Telephone +1 707-766-6000</li>*/}
-              {/*      <li>Toll-Free 800-989-0201</li>*/}
-              {/*    </ul>*/}
-
-              {/*  </div>*/}
-              {/*  <div className="location showroom">*/}
-              {/*    <h4>Digilock Showroom</h4>*/}
-              {/*    <ul className={'unstyled-list'}>*/}
-              {/*      <li>theMart | Chicago, IL 60654<br />United States</li>*/}
-              {/*      <li>&nbsp;</li>*/}
-              {/*      <li>*/}
-              {/*        <a href={CompanySettings.SALES_EMAIL}>*/}
-              {/*          <Trans>email_sales</Trans> / US*/}
-              {/*        </a>*/}
-              {/*      </li>*/}
-              {/*      <li>Telephone +1 707-766-6000</li>*/}
-              {/*      <li>Toll-Free 800-989-0201</li>*/}
-              {/*    </ul>*/}
-              {/*  </div>*/}
-
-              {/*  <div className="location americas">*/}
-              {/*    <h4>Digilock Europe BV</h4>*/}
-              {/*    <ul className={'unstyled-list'}>*/}
-              {/*      <li>The Netherlands</li>*/}
-              {/*      <li>&nbsp;</li>*/}
-              {/*      <li>*/}
-              {/*        <a href={CompanySettings.SALES_EMAIL_EUROPE}>*/}
-              {/*          <Trans>email_sales</Trans> / Europe*/}
-              {/*        </a>*/}
-              {/*      </li>*/}
-              {/*      <li>Telephone +31 0 20 303 3060</li>*/}
-              {/*    </ul>*/}
-              {/*  </div>*/}
-              {/*  <div className="location americas">*/}
-              {/*    <h4>Digilock Asia</h4>*/}
-              {/*    <ul className={'unstyled-list'}>*/}
-              {/*      <li>Hong Kong</li>*/}
-              {/*      <li>&nbsp;</li>*/}
-
-              {/*      <li>*/}
-              {/*        <a href={CompanySettings.SALES_EMAIL_ASIA}>*/}
-              {/*          <Trans>email_sales</Trans> / Asia*/}
-              {/*        </a>*/}
-              {/*      </li>*/}
-              {/*      <li>Telephone +852 27776 0800</li>*/}
-              {/*    </ul>*/}
-              {/*  </div>*/}
-              {/*</div>*/}
             </div>
           </section>
           <div id={'partners'} className="section-delimeter container" />
