366daae2e0a52b3caf2efc221f826da592eb0c7a
Dan Wade <daniel.wade@digilock.com>
2022-06-30 07:47:14 -0700

    updating support form name when Fetch

--- site/src/constants/company/settings.js
+++ site/src/constants/company/settings.js
@@ -1,5 +1,6 @@
 module.exports = {
   CONTACT_FORM_ENDPOINT: 'https://api.hsforms.com/submissions/v3/integration/submit/6919241/68323449-a636-4c07-9d63-eb8159325116',
+  CONTACT_FORM_SUPPORT: 'https://api.hsforms.com/submissions/v3/integration/submit/6919241/68323449-a636-4c07-9d63-eb8159325116',
   CONTACT_FORM_PRIVACY: 'https://api.hsforms.com/submissions/v3/integration/submit/6919241/84bb0e4e-122a-4373-930b-e82303ed58f1',
   MARKETING_EMAIL: 'marketing@digilock.com',
   SALES_EMAIL: 'sales@digilock.com',
