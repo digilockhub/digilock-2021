7b23d079f55dcd59df4c87001ee7e4b0e1796c99
Dan Wade <daniel.wade@digilock.com>
2022-07-05 11:21:29 -0700

    adding osano

--- site/gatsby-ssr.js
+++ site/gatsby-ssr.js
@@ -4,4 +4,21 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+const React = require("react")
+exports.onRenderBody = ({
+                          setHeadComponents
+                        }) => {
+  setHeadComponents([
+    <script
+        key="1"
+        type="text/javascript"
+        src="https://cmp.osano.com/169lVRSmxyYsO3WPy/98ece184-4165-4026-a287-d3876bd45a4c/osano.js"
+    />,
+    <script
+        key="2"
+        type="text/javascript"
+        src="//js.hsforms.net/forms/v2.js"
+    />
+  ])
+}
+
