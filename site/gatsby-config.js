1d3a23c4d71c3824950d82e684eb99d0f88c3ca3
Dan Wade <daniel.wade@digilock.com>
2022-07-18 10:41:44 -0700

    adding google and commenting

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -12,6 +12,27 @@ module.exports = {
       resolve: `gatsby-plugin-sass`,
     },
     // {
+    //   resolve: `gatsby-plugin-gtag`,
+    //   options: {
+    //     // your google analytics tracking id
+    //     trackingId: `UA-23465209-1`,
+    //     // Puts tracking script in the head instead of the body
+    //     head: true,
+    //     // enable ip anonymization
+    //     anonymize: true,
+    //   },
+    // },
+    // {
+    //   resolve: `gatsby-plugin-google-tagmanager`,
+    //   options: {
+    //     id: 'GTM-PZDD9H',
+    //     // Include GTM in development.
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: false,
+    //     defaultDataLayer: { platform: 'gatsby' },
+    //   },
+    // },
+    // {
     //   resolve: `gatsby-plugin-linkedin-insight`,
     //   options: {
     //     partnerId: `1643266`,
