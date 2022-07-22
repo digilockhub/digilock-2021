65d5b8dea4c9f58609e58767eb60f45cd6bc811c
Dan Wade <daniel.wade@digilock.com>
2022-07-22 10:00:38 -0700

    documents

--- site/src/pages/support/smart-locks/aspire/Tab.js
+++ site/src/pages/support/smart-locks/aspire/Tab.js
@@ -4,21 +4,61 @@ import {Trans} from "gatsby-plugin-react-i18next";
 import Accordion from '../../../../components/ui/legacy/Accordion/Accordion'
 
 
-let brochuresKeypad = [
+let dataSheetsKeypad = [
   {
-    "title": "Aspire Brochure",
+    "title": "Smart Keypad with Mobile ID | Deadbolt Horizontal Body",
     "file": {
-      "url": "#",
-      "fileName": "DS-Aspire-Dual-Keypad.pdf",
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/3g8DPRw6hJZOJcbjwwgl4T/64c9165e4af9bcf2e02397e65581a510/DS-D6AKN-H-Bolt-Cable-DEN.pdf",
+      "fileName": "DS-D6AKN-H-Bolt-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart Keypad with Mobile ID | Deadlatch Horizontal Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/1CxE0P8tuMRLsSnhzAhPE2/d307e9f23a24e675bdb8576e6d7a6d0b/DS-D6AKN-H-Latch-Cable-DEN.pdf",
+      "fileName": "DS-D6AKN-H-Latch-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart Keypad with Mobile ID | Deadbolt Standard Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/3XaWKP83cMGDOr1HPGbKFn/653e065885346832bd12cdbc2aba9796/DS-D6AKN-S-Bolt-Cable-DEN.pdf",
+      "fileName": "DS-D6AKN-S-Bolt-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart Keypad with Mobile ID | Deadlatch Standard Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/Wn1Wl6di1KH05VRqJmnvV/3b94c8f97f90e7298ac8f1523e0cffe9/DS-D6AKN-S-Latch-Cable-DEN.pdf",
+      "fileName": "DS-D6AKN-S-Latch-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart Keypad with Mobile ID | Deadbolt Vertical Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/1J84GzmZ7AdggNdsQFGJPM/aaa4c9eba84842ee23100cf976345314/DS-D6AKN-V-Bolt-Cable-DEN.pdf",
+      "fileName": "DS-D6AKN-V-Bolt-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart Keypad with Mobile ID | Deadlatch Vertical Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/4zynjc4UWMAcQHGzKuAPOU/7d7743c759bbc184334f8d5341440694/DS-D6AKN-V-Latch-Cable-DEN.pdf",
+      "fileName": "DS-D6AKN-V-Latch-Cable-DEN.pdf",
       "contentType": "application/pdf"
     }
   }
 ];
 let installGuidesKeypad = [
   {
-    "title": "Aspire Install Guice",
+    "title": "Install Guide Keypad & RFID",
     "file": {
-      "url": "https://downloads.ctfassets.net/q5vaqi9g69zw/5OopVVVMyPXOmt8GNlBVqR/ac1d0db9e6dfe6e8767b822af8d7b028/IG-D6AN-KR-DEN.pdf",
+      "url": "https://downloads.ctfassets.net/q5vaqi9g69zw/2PkM8fC8UcM6Me6uA7DIK1/6ccaca155307b00ead1745d4af720e8c/IG-D6AN-KR-DEN.pdf",
       "fileName": "IG-D6AN-KR-DEN.pdf",
       "contentType": "application/pdf"
     }
@@ -26,28 +66,52 @@ let installGuidesKeypad = [
 ];
 let productGuidesKeypad = [
   {
-    "title": "Aspire Product Guide Keypad Dual",
+    "title": "Keypad Shared & Assigned Use Functionality",
     "file": {
-      "url": "//downloads.ctfassets.net/q5vaqi9g69zw/nOj1Z5wMrD7h7m6vV2XCy/142a552177b8c4ded9b449f0d9e0771d/PG-Aspire-Dual-Keypad.pdf",
-      "fileName": "PG-Aspire-Dual-Keypad.pdf",
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/4Ym8u81r95Xh049BIY1xH6/33ec7b5073b95453ac737e4e3bf6271a/PG-D6AKN-D-DEN.pdf",
+      "fileName": "PG-D6AKN-D-DEN.pdf",
       "contentType": "application/pdf"
     }
   },
   {
-    "title": "Aspire Product Guide Keypad Assigned",
+    "title": "Keypad Assigned Use Functionality",
     "file": {
-      "url": "//assets.ctfassets.net/q5vaqi9g69zw/7bil0w8KuO7px6epQ3vBob/5fd7256d1b2160a436550baf326f393c/PG-Aspire-Assigned-Keypad.pdf",
-      "fileName": "PG-Aspire-Assigned-Keypad.pdf",
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/33ghd28ihDurpvtHSsCrji/1d8e482fec529b86133685f8f0d47b40/PG-D6AKN-P-DEN.pdf",
+      "fileName": "PG-D6AKN-P-DEN.pdf",
       "contentType": "application/pdf"
     }
   }
 ];
-let brochuresRFID = [
+let dataSheetsRFID = [
   {
-    "title": "Aspire Brochure",
+    "title": "Smart RFID with Mobile ID | Deadbolt Narrow Body",
     "file": {
-      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/5l4Od8Jce0NCYqDepMHYK5/bb49d1a1fb6fbf1641c6a4d55351d90b/DIGILOCK-ASPIRE-RFID-NETWORK-PRODUCT-BROCHURE.pdf",
-      "fileName": "DIGILOCK-ASPIRE-RFID-NETWORK-PRODUCT-BROCHURE.pdf",
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/7byhJKWiHkzuazpZFECOy6/a20f29f9d2a240c4255c2b4f04891268/DS-D6ARN-N-Bolt-Cable-DEN.pdf",
+      "fileName": "DS-D6ARN-N-Bolt-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart RFID with Mobile ID | Deadlatch Narrow Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/5UiWVUZ7e6XAnWRBawzNTT/1262766d46e26ff414072b2c78be32a8/DS-D6ARN-N-Latch-Cable-DEN.pdf",
+      "fileName": "DS-D6ARN-N-Latch-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart RFID with Mobile ID | Deadlatch Narrow Body",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/2tO3AlhYs0V8kR3nFsEaLb/bf6c4025e3cd40f6f65ed42a852fc660/DS-D6ARN-S-Bolt-Cable-DEN.pdf",
+      "fileName": "DS-D6ARN-S-Bolt-Cable-DEN.pdf",
+      "contentType": "application/pdf"
+    }
+  },
+  {
+    "title": "Smart RFID with Mobile ID | Deadlatch",
+    "file": {
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/12IL7G3vJdSoeaX1EIQhLf/6ba841cbdd4426e9348a4f9ea19c8412/DS-D6ARN-S-Latch-Cable-DEN.pdf",
+      "fileName": "DS-D6ARN-S-Latch-Cable-DEN.pdf",
       "contentType": "application/pdf"
     }
   }
@@ -64,18 +128,18 @@ let installGuidesRFID = [
 ];
 let productGuidesRFID = [
   {
-    "title": "Aspire Product Guide RFID Assigned",
+    "title": "Touch-Free RFID Shared Use Functionality",
     "file": {
-      "url": "//assets.ctfassets.net/q5vaqi9g69zw/4f03m4CJmZICQbslTfHZdQ/7325c5f34cceb64689c72d4d4ee08814/PG-Aspire-Assigned-RFID.pdf",
-      "fileName": "PG-Aspire-Assigned-RFID.pdf",
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/T1hWORlXmQfCmOTnn5Ogp/49a6e0a4dbae7dd4284c166ce779a745/PG-D6ARN-D-DEN.pdf",
+      "fileName": "PG-D6ARN-D-DEN.pdf",
       "contentType": "application/pdf"
     }
   },
   {
-    "title": "Aspire Product Guide RFID Shared",
+    "title": "Touch-Free RFID Assigned Use Functionality",
     "file": {
-      "url": "//assets.ctfassets.net/q5vaqi9g69zw/6EnMGYYmp9U9x9qaHINVN0/2f3a9ea4ed72e26c2e7a11f43bf54868/PG-Aspire-Shared-RFID.pdf",
-      "fileName": "PG-Aspire-Shared-RFID.pdf",
+      "url": "https://assets.ctfassets.net/q5vaqi9g69zw/3lAzXos9f329RZD7BBj5MW/5e5f4d490231b31d1e92fbc46f61c3ba/PG-D6ARN-P-DEN.pdf",
+      "fileName": "PG-D6ARN-P-DEN.pdf",
       "contentType": "application/pdf"
     }
   }
@@ -133,11 +197,11 @@ const Tab = (props) => {
               {/*DOCUMENTS*/}
               <div label="Document Library">
                 <Accordion clsName={'next-support'}>
-                  <div label="Brochures">
+                  <div label="Data Sheets">
                     <div className="step-images">
                       <ul>
                         {
-                          brochuresKeypad.map((doc, index) => (
+                          dataSheetsKeypad.map((doc, index) => (
                                   <li key={index}>
                                     <li key={index}>
                                       <a href={doc.file.url} target={'_blank'}>{doc.title}</a>
@@ -154,6 +218,20 @@ const Tab = (props) => {
                       <ul>
                         {
                           installGuidesKeypad.map((doc, index) => (
+                                  <li key={index}>
+                                    <a href={doc.file.url} target={'_blank'}>{doc.title}</a>
+                                  </li>
+                              )
+                          )
+                        }
+                      </ul>
+                    </div>
+                  </div>
+                  <div label="Product Guides">
+                    <div className="step-images">
+                      <ul>
+                        {
+                          productGuidesKeypad.map((doc, index) => (
                                   <li key={index}>
                                     <a href={doc.file.url} target={'_blank'}>{doc.title}</a>
                                   </li>
@@ -192,11 +270,11 @@ const Tab = (props) => {
                 {/*DOCUMENTS*/}
                 <div label="Document Library">
                   <Accordion clsName={'next-support'}>
-                    <div label="Brochures">
+                    <div label="Data Sheets">
                       <div className="step-images">
                         <ul>
                           {
-                            brochuresRFID.map((doc, index) => (
+                            dataSheetsRFID.map((doc, index) => (
                                     <li key={index}>
                                       <li key={index}>
                                         <a href={doc.file.url} target={'_blank'}>{doc.title}</a>
@@ -222,20 +300,20 @@ const Tab = (props) => {
                         </ul>
                       </div>
                     </div>
-                    {/*<div label="Product Guides">*/}
-                    {/*  <div className="step-images">*/}
-                    {/*    <ul>*/}
-                    {/*      {*/}
-                    {/*        productGuidesRFID.map((doc, index) => (*/}
-                    {/*                <li key={index}>*/}
-                    {/*                  <a href={doc.file.url} target={'_blank'}>{doc.title}</a>*/}
-                    {/*                </li>*/}
-                    {/*            )*/}
-                    {/*        )*/}
-                    {/*      }*/}
-                    {/*    </ul>*/}
-                    {/*  </div>*/}
-                    {/*</div>*/}
+                    <div label="Product Guides">
+                      <div className="step-images">
+                        <ul>
+                          {
+                            productGuidesRFID.map((doc, index) => (
+                                    <li key={index}>
+                                      <a href={doc.file.url} target={'_blank'}>{doc.title}</a>
+                                    </li>
+                                )
+                            )
+                          }
+                        </ul>
+                      </div>
+                    </div>
                   </Accordion>
                 </div>
             </div>
